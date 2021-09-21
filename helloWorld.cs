50e31d8cd508099222a7fb72029acb3c1278b929
XDomeksX <90246598+XDomeksX@users.noreply.github.com>
2021-09-21 10:50:02 +0200

    Create helloWorld.cs

---
+++ helloWorld.cs
@@ -0,0 +1,16 @@
+/*
+  Domenik Milohanić
+  3.d
+  21.9.2021.
+  hello world
+*/
+
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Hello World");
+	}
+}
